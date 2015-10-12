a060d01436479214e5627314a22cd34f05f1573d
nathanjsweet <nathanjsweet@gmail.com>
2015-10-12 16:38:07 -0700

    mv

--- build-release.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-id=$(docker create njs0/gohunservice:build)
-docker cp $id:/go/bin/gohunservice - > gohunservice.tar
-docker rm -v $id
-tar -xvf gohunservice.tar gohunservice
-rm gohunservice.tar
-docker build -t njs0/gohunservice:release -f Dockerfile.release .
-rm gohunservice
